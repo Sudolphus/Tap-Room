009c4e9715d13734a0a6c9e379d6f16d5ea9e1a3
Micheal Hansen <micheal.hansen@gmail.com>
2020-09-11 16:36:06 -0700

    add modal to confirm a delete

--- src/components/DrinkDetails.js
+++ src/components/DrinkDetails.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 
 function DrinkDetails(props) {
   const { onLinkClick, onDelete, drink } = props;
   const { name, brand, price, alcoholContent, quantity } = drink;
+  const [show, setShow] = useState(false);
+  const handleShow = () => setShow(true);
+  const handleClose = () => setShow(false);
+
   return (
     <React.Fragment>
       <h3>{name} --- ${price}</h3>
@@ -13,7 +18,18 @@ function DrinkDetails(props) {
       <p><i>{quantity} remaining</i></p>
       <Button variant='secondary' type='button' size='lg' block onClick={()=>onLinkClick('index')}>Back To Index</Button>
       <Button variant='warning' type='button' size='lg' block onClick={()=>onLinkClick('edit', drink)}>Edit Drink</Button>
-      <Button variant='danger' type='button' size='lg' block onClick={()=>onDelete(drink)}>Delete Drink</Button>
+      <Button variant='danger' type='button' size='lg' block onClick={handleShow}>Delete Drink</Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Confirm Delete?</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Are you sure you want to delete {name}?</Modal.Body>
+        <Modal.Footer>
+          <Button variant='secondary' type='button' onClick={handleClose}>&times;</Button>
+          <Button variant='primary' type='button' onClick={()=>onDelete(drink)}>&#10004;</Button>
+        </Modal.Footer>
+      </Modal>
     </React.Fragment>
   )
 }
