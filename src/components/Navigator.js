cbb83be014c6894d3b0aa672d388cd8747aae5b5
Micheal Hansen <micheal.hansen@gmail.com>
2020-09-11 18:24:02 -0700

    change the navigator to a dropdown, because I like that better

--- src/components/Navigator.js
+++ src/components/Navigator.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 import LogoSmall from './img/LogoSmall.png';
 import './css/Navigator.css';
 
 function Navigator(props) {
   const onLinkClick = props.onLinkClick;
-  
+  const whiteLink = {
+    color: "white"
+  }
   return (
     <Navbar className="mb-3 custom-nav">
       <Navbar.Brand><img src={LogoSmall} alt='taphouse logo' className="brand-img" /></Navbar.Brand>
-      <Nav className='mr-auto'>
-        <Nav.Link className="custom-link" onClick={()=>onLinkClick('index')}>Main</Nav.Link>
-        <Nav.Link className="custom-link" onClick={()=>onLinkClick('create')}>Add</Nav.Link>
-      </Nav>
+      <NavDropdown title='Some Handy Links' id='basic-nav-dropdown' className='mr-auto float-left custom-link' style={whiteLink}>
+        <NavDropdown.Item onClick={()=>onLinkClick('index')}>Main List</NavDropdown.Item>
+        <NavDropdown.Item onClick={()=>onLinkClick('create')}>Add a Beer</NavDropdown.Item>
+      </NavDropdown>
     </Navbar>
   )
 }
