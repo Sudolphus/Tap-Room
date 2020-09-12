e4a61219b7429057bc5f1d267a450ffe83cbdc5d
Micheal Hansen <micheal.hansen@gmail.com>
2020-09-11 18:24:59 -0700

    play around with inline styles'

--- src/components/Navigator.js
+++ src/components/Navigator.js
@@ -7,13 +7,11 @@ import './css/Navigator.css';
 
 function Navigator(props) {
   const onLinkClick = props.onLinkClick;
-  const whiteLink = {
-    color: "white"
-  }
+  
   return (
     <Navbar className="mb-3 custom-nav">
       <Navbar.Brand><img src={LogoSmall} alt='taphouse logo' className="brand-img" /></Navbar.Brand>
-      <NavDropdown title='Some Handy Links' id='basic-nav-dropdown' className='mr-auto float-left custom-link' style={whiteLink}>
+      <NavDropdown title='Some Handy Links' id='basic-nav-dropdown' className='mr-auto float-left custom-link'>
         <NavDropdown.Item onClick={()=>onLinkClick('index')}>Main List</NavDropdown.Item>
         <NavDropdown.Item onClick={()=>onLinkClick('create')}>Add a Beer</NavDropdown.Item>
       </NavDropdown>
