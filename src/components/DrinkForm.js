c368d37ed63e327a1a70e8bf87346af24bc9e748
Micheal Hansen <micheal.hansen@gmail.com>
2020-09-11 17:24:52 -0700

    add button group to form to make it prettier

--- src/components/DrinkForm.js
+++ src/components/DrinkForm.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup'
 import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import './css/DrinkForm.css';
@@ -24,47 +25,47 @@ function DrinkForm(props) {
     returnButton = <Button variant='info' type='button' size='lg' block onClick={()=>onLinkClick('details', originalDrink)}>Back To Drink</Button>
   }
   return (
-    <React.Fragment>
-      <Form onSubmit={handleSubmittingForm}>
-        <Form.Group controlId='name'>
-          <Form.Label>Name</Form.Label>
-          <Form.Control type='text' placeholder='Product Name' defaultValue={defaultName} required />
-        </Form.Group>
-        <Form.Group controlId='brand'>
-          <Form.Label>Brand</Form.Label>
-          <Form.Control type='text' placeholder='Brand' defaultValue={defaultBrand} required />
-        </Form.Group>
-        <Row>
-          <Col>
-            <Form.Group controlId='price'>
-              <Form.Label>Price</Form.Label>
-              <InputGroup>
-                <InputGroup.Prepend>
-                  <InputGroup.Text>$</InputGroup.Text>
-                </InputGroup.Prepend>
-                <Form.Control type='text' placeholder='Price' defaultValue={defaultPrice} pattern='^\d+\.\d{2}$' required />
-              </InputGroup>
-              <Form.Text className="text-muted">Price takes a decimal input (e.g. 5.50 or 12.00)</Form.Text>
-            </Form.Group>
-          </Col>
-          <Col>
-            <Form.Group controlId='content'>
-              <Form.Label>Alcohol Content</Form.Label>
-              <InputGroup>
-                <Form.Control type='text' placeholder='Content' defaultValue={defaultContent} pattern='^\d{1,2}\.\d+$' required />
-                <InputGroup.Append>
-                  <InputGroup.Text>%</InputGroup.Text>
-                </InputGroup.Append>
-              </InputGroup>
-              <Form.Text className='text-muted'>Alcohol Content takes a decimal input (e.g. 5.5 or 6.75)</Form.Text>
-            </Form.Group>
-          </Col>
-        </Row>
+    <Form onSubmit={handleSubmittingForm}>
+      <Form.Group controlId='name'>
+        <Form.Label>Name</Form.Label>
+        <Form.Control type='text' placeholder='Product Name' defaultValue={defaultName} required />
+      </Form.Group>
+      <Form.Group controlId='brand'>
+        <Form.Label>Brand</Form.Label>
+        <Form.Control type='text' placeholder='Brand' defaultValue={defaultBrand} required />
+      </Form.Group>
+      <Row>
+        <Col>
+          <Form.Group controlId='price'>
+            <Form.Label>Price</Form.Label>
+            <InputGroup>
+              <InputGroup.Prepend>
+                <InputGroup.Text>$</InputGroup.Text>
+              </InputGroup.Prepend>
+              <Form.Control type='text' placeholder='Price' defaultValue={defaultPrice} pattern='^\d+\.\d{2}$' required />
+            </InputGroup>
+            <Form.Text className="text-muted">Price takes a decimal input (e.g. 5.50 or 12.00)</Form.Text>
+          </Form.Group>
+        </Col>
+        <Col>
+          <Form.Group controlId='content'>
+            <Form.Label>Alcohol Content</Form.Label>
+            <InputGroup>
+              <Form.Control type='text' placeholder='Content' defaultValue={defaultContent} pattern='^\d{1,2}\.\d+$' required />
+              <InputGroup.Append>
+                <InputGroup.Text>%</InputGroup.Text>
+              </InputGroup.Append>
+            </InputGroup>
+            <Form.Text className='text-muted'>Alcohol Content takes a decimal input (e.g. 5.5 or 6.75)</Form.Text>
+          </Form.Group>
+        </Col>
+      </Row>
+      <ButtonGroup vertical size='lg'>
         <Button variant='success' type='submit' size='lg' block>{buttonText}</Button>
-      </Form>
-      {returnButton}
-      <Button variant='secondary' type='button' size='lg' block onClick={()=>onLinkClick('index')}>Back To Index</Button>
-    </React.Fragment>
+        {returnButton}
+        <Button variant='secondary' type='button' size='lg' block onClick={()=>onLinkClick('index')}>Back To Index</Button>
+      </ButtonGroup>
+    </Form>
   )
 }
 
